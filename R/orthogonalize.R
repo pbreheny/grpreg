ebc850bd6459e0622930cfbcd130696dcb126f64
Patrick Breheny <patrick.breheny@uky.edu>
2013-02-10 07:51:18 -0500

    Version 2.3-0: Cholesky -> SVD

--- R/orthogonalize.R
+++ R/orthogonalize.R
@@ -4,7 +4,6 @@ orthogonalize <- function(X, group)
   J <- max(group)
   T <- vector("list", J)
   XX <- matrix(0, nrow=nrow(X), ncol=ncol(X))
-  ##Uinv <- vector("list", J)
   XX[,which(group==0)] <- X[,which(group==0)]
   for (j in 1:J) {
     ind <- which(group==j)
@@ -13,16 +12,11 @@ orthogonalize <- function(X, group)
     r <- which(SVD$d > 1e-10)
     T[[j]] <- sweep(SVD$u[,r,drop=FALSE], 2, SVD$d[r]^(-1/2), "*")
     XX[,ind[r]] <- X[,ind]%*%T[[j]]
-    ##result <- try(U <- chol(crossprod(X[, ind, drop=FALSE])/n))
-    ##if (class(result)=="try-error") stop(paste("Group ", j," is not full-rank"))
-    ##Uinv[[j]] <- backsolve(U,diag(length(ind)))
-    ##XX[,ind] <- X[,ind] %*% Uinv[[j]]
   }
   nz <- !apply(XX==0,2,all)
   XX <- XX[, nz, drop=FALSE]
   attr(XX, "T") <- T
   attr(XX, "group") <- group[nz]
-  ##attr(XX,"Uinv") <- Uinv
   XX
 }
 unorthogonalize <- function(b, XX, group.full, group)
