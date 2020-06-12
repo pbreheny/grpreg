123a2e9ffbed68a38d440a2b718789c11326ab87
Patrick Breheny <patrick-breheny@uiowa.edu>
2020-06-12 14:10:56 -0500

    Renaming test directory

--- tests/test.R
+++ tests/test.R
@@ -1,5 +1,5 @@
 if (requireNamespace("tinytest", quietly=TRUE)) {
   if (length(unclass(packageVersion("grpreg"))[[1]]) == 4) {
-    tinytest::test_package("grpreg", "tests")
+    tinytest::test_package("grpreg", pattern="^[^_].*\\.[rR]$")
   }
 }
